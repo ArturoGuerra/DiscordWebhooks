52626627139c29a0278c41adaf41dde258d933aa
ArturoGuerra <arturoguerra360@gmail.com>
2017-04-20 02:58:16 +0000

    Its a package

--- discordwebhooks/discordwebhooks.py
+++ discordwebhooks/discordwebhooks.py
@@ -46,18 +46,45 @@ class Embeds():
     def add_fields(self, field):
         if field.__class__.__name__ == "Field":
             self.embed['fields'] = field.content()
-    def add_footer(self, footer):
-        pass
-    def add_video(self, video):
-        pass
-    def add_image(self, image):
-        pass
-    def add_author(self, author):
-        pass
-    def add_provider(self, provider):
-        pass
-    def add_thumbnail(self, thumbnail):
-        pass
+    def add_footer(self, text, icon_url=None):
+        footer = dict()
+        footer['text'] = text
+        if icon_url:
+            footer['icon_url'] = icon_url
+        self.embed['footer'] = footer
+    def add_video(self, url, height, width):
+        video = dict()
+        video['url'] = url
+        video['width'] = width
+        video['height'] = height
+        self.embed['video'] = video
+    def add_image(self, url, height, width):
+        image = dict()
+        image['url'] = url
+        image['width'] = width
+        image['height'] = height
+        self.embed['image'] = image
+    def add_author(self, name, url=None, icon_url=None):
+        author = dict()
+        author['name'] = name
+        if icon_url:
+            author['icon_url'] = icon_url
+        if url:
+            author['url'] = url
+        self.embed['author'] = author
+    def add_provider(self, name, url):
+        provider = dict()
+        provider['url'] = url
+        provider['name'] = name
+        self.embed['provider'] = provider
+    def add_thumbnail(self, url, height, width, proxy_url=None):
+        thumbnail = dict()
+        thumbnail['url'] = url
+        thumbnail['height'] = height
+        thumbnail['width'] = width
+        if proxy_url:
+            thumbnail['proxy_url'] = proxy_url
+        self.embed['thumbnail'] = thumbnail
     def content(self):
         return self.embed
 
